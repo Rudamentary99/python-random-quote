e369f84413362ed2ec25b3ce117488df556ed1fb
Rudamentary99 <nathan.mccloskey@icloud.com>
2020-07-18 22:14:47 -0500

    added rnd

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,12 @@
+import random
 def newerName():
-  print("Keep it logically awesome.")
+  #print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  rnd = random.randint(0, len(quotes) - 1)
+  print(quotes[rnd])
 
 if __name__== "__main__":
   newerName()
